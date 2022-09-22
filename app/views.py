2022f97db037041d05efdc3507ed749d5d27dae7
yusukezemi <nakami3316@gmail.com>
2022-09-22 12:58:42 +0900

    問題追加機能の実装

--- app/views.py
+++ app/views.py
@@ -15,3 +15,38 @@ def move_to_fuseikaipage(request):
 
 def monndai2_show(request):
     return render(request,'monndai2.html')
+
+def move_to_AddText(request):
+    return render(request,'AddText.html')
+
+# #下記はtodoを参考にした
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+# from django.views.generic import CreateView
+
+from django.urls import reverse_lazy
+
+from .models import Quiz
+
+# # class QuizList(ListView):
+# #     model = Quiz
+# #     context_object_name = "tasks"
+
+
+# # class QuizDetail(DetailView):
+# #     model = Quiz
+# #     context_object_name = "task"
+
+class TodoCreate(CreateView):
+    model = Quiz
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+from django.views.generic import ListView #追加
+
+from .models import Quiz
+
+
+class TodoList(ListView):
+    model = Quiz
+    context_object_name = "tasks"
